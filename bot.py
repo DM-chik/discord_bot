c5509a4aaaabd9932765b26d6a62d9a17c8b6ea8
DM-chik <mardmal07@mail.ru>
2025-07-17 17:00:03 +0600

    Hello World

---
+++ bot.py
@@ -0,0 +1,32 @@
+from cogs import Cogs
+import disnake
+import os
+from dotenv import load_dotenv
+from disnake.ext import commands
+
+load_dotenv()
+intents = disnake.Intents.all()
+bot = commands.Bot(command_prefix="!", test_guilds=[867424312897306676], intents=intents)
+bot_token = os.getenv("bot_token")
+bot.add_cog(Cogs(bot))
+
+
+@bot.event
+async def on_ready():
+    print(f"Бот {bot.user} готов!")
+
+
+@bot.event
+async def on_member_join(member):
+    role = await disnake.utils.get(id_guild=867424312897306676, role_id=1278589046116978761)
+    channel = bot.get_channel(867424313408618507)
+    embed = disnake.Embed(
+        title="Новый участник",
+        description=f"{member.name}",
+        color=0xffffff
+    )
+    await member.add_role(role)
+    await channel.send(embed=embed)
+
+
+bot.run(bot_token)
---
+++ cogs.py
@@ -0,0 +1,125 @@
+from disnake.ext import commands
+from disnake import ClientException
+from disnake import ApplicationCommandInteraction
+from disnake import FFmpegOpusAudio
+from disnake import VoiceChannel
+
+ffmpeg_path = "E:\\ffmpeg-7.1.1-essentials_build\\bin\\ffmpeg.exe"
+
+
+class Cogs(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.slash_command()
+    async def hello(self, inter):
+        await inter.response.send_message('Хуй соси')
+
+    # удаление запрещённых слов
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author != self.bot.user:
+            for i in message.content.split():
+                for j in open('bad_words', encoding="utf-8"):
+                    if j.lower()[:-1] == i.lower():
+                        await message.delete()
+                        await message.channel.send(f'{message.author.mention} долбаёб')
+                        return
+
+    @commands.slash_command()
+    async def ping(self, inter):
+        await inter.response.send_message('Понг!')
+
+    @commands.slash_command()
+    async def play(self, inter: ApplicationCommandInteraction, audio_file: str):
+        if inter.author.voice is None:
+            await inter.response.send_message('Вы не находитесь в голосовом каналеэ')
+            return
+        channel = inter.author.voice.channel
+        voice_client = inter.guild.voice_client
+        if voice_client is None:
+            try:
+                voice_client = await channel.connect()
+            except ClientException as e:
+                await inter.response.send_message(f'Ошибка подключения: {e}', ephemeral=True)
+                return
+            except Exception as e:
+                await inter.response.send_messade(f'Неизвестная ошибка подключения: {e}', ephemeral=True)
+                return
+        if voice_client.channel != channel:
+            await inter.response.send_message('Я уже подключен к другому голосовому каналу', ephemeral=True)
+            return
+        try:
+            with open('sounds/' + audio_file, 'rb'):
+                pass
+        except FileNotFoundError:
+            await inter.response.send_message(f'Файл {audio_file} не найден', ephemeral=True)
+            return
+        try:
+            if voice_client.is_playing():
+                voice_client.stop()
+            audio_source = FFmpegOpusAudio('sounds/' + audio_file, executable=ffmpeg_path)
+            voice_client.play(audio_source)
+            await inter.response.send_message('Звуковой файл воспроизводиться', ephemeral=True)
+        except Exception as e:
+            await inter.response.send_message(f'Ошибка воспроизведения: {e}', ephemeral=True)
+
+    @commands.slash_command()
+    async def stop_playing(self, inter: ApplicationCommandInteraction):
+        if inter.author.voice is None:
+            await inter.response.send_message('Вы не находитесь в голосовом каналеэ', ephemeral=True)
+            return
+        voice_client = inter.guild.voice_client
+        if voice_client is None:
+            await inter.response.send_message('Бот не находится в голосовом канале', ephemeral=True)
+            return
+        if inter.author.voice.channel != voice_client.channel:
+            await inter.response.send_message('Бот не находится в вашем голосовом канале', ephemeral=True)
+            return
+        try:
+            if voice_client.is_playing():
+                voice_client.stop()
+                await inter.response.send_message('Конец воспроизведения', ephemeral=True)
+                return
+            else:
+                await inter.response.send_message('Сейчас ничего не играет', ephemeral=True)
+                return
+        except Exception as e:
+            await inter.response.send_message(f'Ошибка остановки: {e}', ephemeral=True)
+            return
+
+    # калькулятор
+    @commands.slash_command()
+    async def score(self, inter, condition):
+        await inter.response.send_message(eval(condition))
+
+    # подключение бота
+    @commands.slash_command()
+    async def join(self, inter: ApplicationCommandInteraction, channel: VoiceChannel = None):
+        if channel is None:
+            if inter.author.voice:
+                channel = inter.author.voice.channel
+                await inter.response.send_message('Бот успешно потключился к голосовому каналу', ephemeral=True)
+            else:
+                await inter.response.send_messages('Вы не находитесь в голосовом канале', ephemeral=True)
+        try:
+            await channel.connect()
+        except ClientException as e:
+            await inter.response.send_message(f'Ошибка подключения {e}', ephemeral=True)
+        except Exception as e:
+            await inter.response.send_messages(f'Ошибка подключения {e}', ephemeral=True)
+
+    # отключение бота
+    @commands.slash_command()
+    async def leave(self, inter: ApplicationCommandInteraction):
+        voice_client = inter.guild.voice_client
+        if self.bot.voice_clients:
+            try:
+                await voice_client.disconnect()
+                await inter.response.send_message('Успешно отключился от голосового канала', ephemeral=True)
+            except Exception as e:
+                await inter.response.send_message(f'Ошибка отключения {e}', ephemeral=True)
+        else:
+            await inter.response.send_messages(f'Бот не подключён к голосовому каналу', ephemeral=True)
+
+
